0d51de305f523ae48fbfaf29dfa34cf6524c6e78
Hardik-khobragade <hardikkhobragade78@gmail.com>
2025-10-07 22:21:31 +0530

    Train and evalution file done

--- train_eval.py
+++ train_eval.py
@@ -27,3 +27,45 @@ def get_dataloaders(dataset_name="CIFAR10",batch_size=128,remove_idx=None):
     test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, num_workers=4)
     return train_loader, test_loader
 
+def build_model(num_classes=10):
+    m= models.resnet18(pretrained=False)
+    m.fc =nn.Linear(m.fc.in_features , num_classes)
+    return m
+
+def train_one_epoch(model, loader, optimizer, criterion, device):
+    model.train()
+    total_loss = 0.0
+    correct = 0
+    total = 0
+    for x,y in loader:
+        x,y= x.to(device), y.to(device)
+        out = model(x)
+        loss = criterion(out,y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * x.size(0)
+        _,pred = out.max(1)
+        correct += (pred==y).sum().item()
+        total += x.size(0)
+    return total_loss/total, correct/total
+
+def evaluation(model, loader, device):
+    model.eval()
+    total=0
+    correct=0
+    losses=[]
+    criterion = nn.CrossEntropyLoss(reduction='none')
+    with torch.no_grad():
+        for x,y in loader:
+            x,y = x.to(device), y.to(device)
+            out = model(x)
+            loss = criterion(out,y)
+            _,pred = out.max(1)
+            correct += (pred==y).sum().item()
+            total += x.size(0)
+            losses.append(loss.cpu())
+    return correct/total, torch.cat(losses)
+            
+    
+
